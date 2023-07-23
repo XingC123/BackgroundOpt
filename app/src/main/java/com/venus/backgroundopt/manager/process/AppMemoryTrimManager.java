532c0391cebcb71c1ebd3d70bdd49705e5149906
XingC <123fengmo@gmail.com>
2023-07-23 12:52:43 +0800

    refactor: 调整前后台内存清理任务的执行方式
    将excutor.scheduleAtFixedRate替换为executor.scheduleWithFixedDelay
    
    (cherry picked from commit b30d4047f169b101e35917c15e5df1c5089f316d)

--- app/src/main/java/com/venus/backgroundopt/manager/process/AppMemoryTrimManager.java
+++ app/src/main/java/com/venus/backgroundopt/manager/process/AppMemoryTrimManager.java
@@ -25,21 +25,19 @@ public abstract class AppMemoryTrimManager implements ILogger {
     public AppMemoryTrimManager() {
         // 在任务取消时一并将其移除
         executor.setRemoveOnCancelPolicy(true);
-        // 设置最大线程数(就目前来说, 只有从前台退往后台以后, 任务才会被添加于此, 安卓的后台, 真的能挂100个吗)
-        executor.setMaximumPoolSize(100);
     }
 
-    public abstract int getDefaultTrimLevel();
+    abstract int getDefaultTrimLevel();
 
-    public abstract String getMemoryTrimManagerName();
+    abstract String getMemoryTrimManagerName();
 
-    public abstract long getTaskInitialDelay();
+    abstract long getTaskInitialDelay();
 
-    public abstract long getTaskPeriod();
+    abstract long getTaskPeriod();
 
-    public abstract TimeUnit getTaskTimeUnit();
+    abstract TimeUnit getTaskTimeUnit();
 
-    private String getMemoryTrimManagerNameImpl() {
+    String getMemoryTrimManagerNameImpl() {
         return getMemoryTrimManagerName() + ": ";
     }
 
@@ -78,7 +76,7 @@ public abstract class AppMemoryTrimManager implements ILogger {
         removeTrimTask(processRecord);  // 移除原有的以充分保持轮循间隔
 
         // 立即执行。每隔3分钟执行
-        ScheduledFuture<?> scheduledFuture = executor.scheduleAtFixedRate(
+        ScheduledFuture<?> scheduledFuture = executor.scheduleWithFixedDelay(
                 runnable,
                 getTaskInitialDelay(),
                 getTaskPeriod(),
