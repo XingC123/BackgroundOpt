233655dcde44b59c12cddf62c770ed7d13d4f16e
XingC <123fengmo@gmail.com>
2023-07-24 19:02:37 +0800

    fix: 修复hook时发生异常未被正常处理的bug

--- app/src/main/java/com/venus/backgroundopt/hook/base/HookPoint.java
+++ app/src/main/java/com/venus/backgroundopt/hook/base/HookPoint.java
@@ -87,9 +87,9 @@ public class HookPoint implements ILogger {
             if (BuildConfig.DEBUG) {
                 getLogger().debug("[" + hookClassName + "." + hookMethodName + "]hook成功");
             }
-        } catch (Exception e) {
+        } catch (Throwable t) {
             if (BuildConfig.DEBUG) {
-                getLogger().debug("[" + hookClassName + "." + hookMethodName + "]hook失败", e);
+                getLogger().error("[" + hookClassName + "." + hookMethodName + "]hook失败", t);
             }
         }
     }
