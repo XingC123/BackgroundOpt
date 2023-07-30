4d92f0583d776e3c1b35f5d6f147944612764f02
XingC <123fengmo@gmail.com>
2023-07-30 18:12:26 +0800

    refactor: 更改ifnn为ifn

--- app/src/main/java/com/venus/backgroundopt/hook/handle/android/ProcessListHook.java
+++ app/src/main/java/com/venus/backgroundopt/hook/handle/android/ProcessListHook.java
@@ -51,92 +51,95 @@ public class ProcessListHook extends MethodHook {
         RunningInfo runningInfo = getRunningInfo();
         AppInfo appInfo = runningInfo.getRunningAppInfo(uid);
 
-        if (appInfo != null) {  // 非系统重要进程
-            int pid = (int) param.args[0];
-            int oomAdjScore = (int) param.args[2];
-            ProcessInfo processInfo;
+        if (appInfo == null) {
+            return null;
+        }
 
-            int mPid = Integer.MIN_VALUE;
+        // 非系统重要进程
+        int pid = (int) param.args[0];
+        int oomAdjScore = (int) param.args[2];
+        ProcessInfo processInfo;
+
+        int mPid = Integer.MIN_VALUE;
+        try {
+            mPid = appInfo.getmPid();
+        } catch (Exception e) {
+            runningInfo.addRunningApp(appInfo);
+
             try {
                 mPid = appInfo.getmPid();
-            } catch (Exception e) {
-                runningInfo.addRunningApp(appInfo);
 
-                try {
-                    mPid = appInfo.getmPid();
-
-                    if (BuildConfig.DEBUG) {
-                        getLogger().debug(appInfo.getPackageName() + "的主进程信息补全完毕");
-                    }
-                } catch (Exception ex) {
-                    if (BuildConfig.DEBUG) {
-                        getLogger().warn("获取: " + appInfo.getPackageName() + " 的mPid出错", ex);
-                    }
+                if (BuildConfig.DEBUG) {
+                    getLogger().debug(appInfo.getPackageName() + "的主进程信息补全完毕");
+                }
+            } catch (Exception ex) {
+                if (BuildConfig.DEBUG) {
+                    getLogger().warn("获取: " + appInfo.getPackageName() + " 的mPid出错", ex);
                 }
             }
+        }
 
-            if (pid == mPid) { // 主进程
-                if (appInfo.getMainProcCurAdj() != ProcessRecord.DEFAULT_MAIN_ADJ) {    // 第一次打开app
-                    param.args[2] = ProcessRecord.DEFAULT_MAIN_ADJ;
+        if (pid == mPid) { // 主进程
+            if (appInfo.getMainProcCurAdj() != ProcessRecord.DEFAULT_MAIN_ADJ) {    // 第一次打开app
+                param.args[2] = ProcessRecord.DEFAULT_MAIN_ADJ;
 
-                    processInfo = appInfo.getmProcessInfo();
-                    processInfo.setFixedOomAdjScore(ProcessRecord.DEFAULT_MAIN_ADJ);
-                    processInfo.setOomAdjScore(oomAdjScore);
+                processInfo = appInfo.getmProcessInfo();
+                processInfo.setFixedOomAdjScore(ProcessRecord.DEFAULT_MAIN_ADJ);
+                processInfo.setOomAdjScore(oomAdjScore);
 
-                    if (BuildConfig.DEBUG) {
-                        getLogger().debug(
-                                "设置主进程[" + pid + "-" + appInfo.getPackageName() + "]adj: "
-                                        + param.args[2]);
-                    }
-                } else {
+                if (BuildConfig.DEBUG) {
+                    getLogger().debug(
+                            "设置主进程[" + pid + "-" + appInfo.getPackageName() + "]adj: "
+                                    + param.args[2]);
+                }
+            } else {
 //                    ProcessRecord mProcessRecord = appInfo.getmProcessRecord();
 //                    mProcessRecord.adjustMaxAdjIfNeed();
 
-                    param.setResult(null);
-                    appInfo.modifyProcessInfoAndAddIfNull(pid, oomAdjScore);
+                param.setResult(null);
+                appInfo.modifyProcessInfoAndAddIfNull(pid, oomAdjScore);
+            }
+        } else if (pid == Integer.MIN_VALUE) {
+            if (BuildConfig.DEBUG) {
+                getLogger().warn(appInfo.getPackageName() + " 的pid = " + pid + " 不符合规范, 无法添加至进程列表");
+            }
+
+            return null;
+        } else { // 子进程的处理
+            // 子进程信息尚未记录
+            if (!appInfo.isRecordedProcessInfo(pid)) {
+                param.args[2] = ProcessRecord.SUB_PROC_ADJ;
+
+                processInfo = appInfo.addProcessInfo(pid, oomAdjScore);
+                processInfo.setFixedOomAdjScore(ProcessRecord.SUB_PROC_ADJ);
+                processInfo.setOomAdjScore(ProcessRecord.SUB_PROC_ADJ);
+
+                if (Objects.equals(AppGroupEnum.IDLE, appInfo.getAppGroupEnum())) {
+                    runningInfo.getProcessManager().setPidToBackgroundProcessGroup(pid, appInfo);
                 }
-            } else if (pid == Integer.MIN_VALUE) {
+
                 if (BuildConfig.DEBUG) {
-                    getLogger().warn(appInfo.getPackageName() + " 的pid = " + pid + " 不符合规范, 无法添加至进程列表");
+                    getLogger().debug(
+                            "设置子进程[" + pid + "-" + appInfo.getPackageName() + "]adj: "
+                                    + param.args[2]);
                 }
+            } else {
+                ProcessInfo subProcessInfo = appInfo.getProcessInfo(pid);
 
-                return null;
-            } else { // 子进程的处理
-                // 子进程信息尚未记录
-                if (!appInfo.isRecordedProcessInfo(pid)) {
-                    param.args[2] = ProcessRecord.SUB_PROC_ADJ;
-
-                    processInfo = appInfo.addProcessInfo(pid, oomAdjScore);
-                    processInfo.setFixedOomAdjScore(ProcessRecord.SUB_PROC_ADJ);
-                    processInfo.setOomAdjScore(ProcessRecord.SUB_PROC_ADJ);
-
-                    if (Objects.equals(AppGroupEnum.IDLE, appInfo.getAppGroupEnum())) {
-                        runningInfo.getProcessManager().setPidToBackgroundProcessGroup(pid, appInfo);
-                    }
-
+                if (subProcessInfo == null) {
                     if (BuildConfig.DEBUG) {
-                        getLogger().debug(
-                                "设置子进程[" + pid + "-" + appInfo.getPackageName() + "]adj: "
-                                        + param.args[2]);
+                        getLogger().warn("子进程 " + appInfo.getPackageName() + "-" + pid + " 为空, 无法调整oom");
                     }
-                } else {
-                    ProcessInfo subProcessInfo = appInfo.getProcessInfo(pid);
-
-                    if (subProcessInfo == null) {
-                        if (BuildConfig.DEBUG) {
-                            getLogger().warn("子进程 " + appInfo.getPackageName() + "-" + pid + " 为空, 无法调整oom");
-                        }
-                        return null;
-                    }
-
-                    int fixedOomAdjScore = subProcessInfo.getFixedOomAdjScore();
-                    // 新的oomAdj小于修正过的adj 或 修正过的adj为不可能取值
-                    if (oomAdjScore < fixedOomAdjScore || fixedOomAdjScore == ProcessList.IMPOSSIBLE_ADJ) {
-                        param.setResult(null);
-                    }
-
-                    appInfo.modifyProcessInfoAndAddIfNull(pid, oomAdjScore);
+                    return null;
                 }
+
+                int fixedOomAdjScore = subProcessInfo.getFixedOomAdjScore();
+                // 新的oomAdj小于修正过的adj 或 修正过的adj为不可能取值
+                if (oomAdjScore < fixedOomAdjScore || fixedOomAdjScore == ProcessList.IMPOSSIBLE_ADJ) {
+                    param.setResult(null);
+                }
+
+                appInfo.modifyProcessInfoAndAddIfNull(pid, oomAdjScore);
             }
         }
 
