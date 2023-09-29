6d80a859c317826b2f2283f74283322528039253
XingC <123fengmo@gmail.com>
2023-09-29 18:28:44 +0800

    feat: 可以通过关键词搜索已安装app

--- app/src/main/kotlin/com/venus/backgroundopt/ui/ShowAllInstalledAppsActivity.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/ui/ShowAllInstalledAppsActivity.kt
@@ -1,6 +1,8 @@
 package com.venus.backgroundopt.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,7 +18,7 @@ import com.venus.backgroundopt.utils.getInstalledPackages
 class ShowAllInstalledAppsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recycler_app_view)
+        setContentView(R.layout.activity_show_all_installer_apps)
 
         showProgressBarViewForAction(this, "正在获取已安装应用...") {
             init()
@@ -24,16 +26,41 @@ class ShowAllInstalledAppsActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        // app展示区
+        val recyclerView = findViewById<RecyclerView>(R.id.showAllInstalledAppsRecycleView)
+        // 初始化搜索栏
+        val searchAppNameText = findViewById<EditText>(R.id.showAllInstalledAppsSearchText)
+        findViewById<Button>(R.id.showAllInstalledAppsSearchBtn).setOnClickListener { _ ->
+            searchAppNameText.text.toString().apply {
+                (recyclerView.adapter as ShowAllInstalledAppsAdapter).filter.filter(this)
+            }
+        }
+
         val appItems = getInstalledPackages(this) { packageInfo ->
             !ActivityManagerService.isImportantSystemApp(packageInfo.applicationInfo)
         }
 
         runOnUiThread {
-            findViewById<RecyclerView>(R.id.recyclerView).apply {
+            recyclerView.apply {
                 layoutManager = LinearLayoutManager(this@ShowAllInstalledAppsActivity).apply {
                     orientation = LinearLayoutManager.VERTICAL
                 }
                 adapter = ShowAllInstalledAppsAdapter(appItems)
+
+                // 设置搜索栏隐藏/显示行为
+//                setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+//                    val firstCompletelyVisibleItemPosition =
+//                        (layoutManager as LinearLayoutManager).findFirstCompletelyVisibleItemPosition()
+//                    if (oldScrollY > 0) { // 上划
+//                        if (firstCompletelyVisibleItemPosition > 1) { // 设置为不可见
+//
+//                        }
+//                    } else if (oldScrollY < 0) { // 下滑
+//                        if (firstCompletelyVisibleItemPosition >= 0) { // 设置为可见
+//
+//                        }
+//                    }
+//                }
             }
         }
     }
--- app/src/main/kotlin/com/venus/backgroundopt/ui/ShowAllInstalledAppsAdapter.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/ui/ShowAllInstalledAppsAdapter.kt
@@ -1,34 +1,41 @@
 package com.venus.backgroundopt.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.venus.backgroundopt.R
 import com.venus.backgroundopt.entity.AppItem
+import com.venus.backgroundopt.utils.StringUtils
 import com.venus.backgroundopt.utils.TMP_DATA
 
 /**
  * @author XingC
  * @date 2023/9/27
  */
-class ShowAllInstalledAppsAdapter(val appItems: List<AppItem>) :
-    RecyclerView.Adapter<ShowAllInstalledAppsAdapter.Companion.ShowAllInstalledAppsViewHolder>() {
-    companion object {
-        class ShowAllInstalledAppsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            var appIcon: ImageView
-            var appName: TextView
+class ShowAllInstalledAppsAdapter(private val appItems: List<AppItem>) :
+    RecyclerView.Adapter<ShowAllInstalledAppsAdapter.ShowAllInstalledAppsViewHolder>(), Filterable {
+    class ShowAllInstalledAppsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var appIcon: ImageView
+        var appName: TextView
 
-            init {
-                appIcon = itemView.findViewById(R.id.installedAppItemAppIcon)
-                appName = itemView.findViewById(R.id.installedAppItemAppNameText)
-            }
+        init {
+            appIcon = itemView.findViewById(R.id.installedAppItemAppIcon)
+            appName = itemView.findViewById(R.id.installedAppItemAppNameText)
         }
     }
 
+    /* *************************************************************************
+     *                                                                         *
+     * ui显示                                                                   *
+     *                                                                         *
+     **************************************************************************/
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -40,11 +47,11 @@ class ShowAllInstalledAppsAdapter(val appItems: List<AppItem>) :
     }
 
     override fun getItemCount(): Int {
-        return appItems.size
+        return filterAppItems.size
     }
 
     override fun onBindViewHolder(holder: ShowAllInstalledAppsViewHolder, position: Int) {
-        val appItem = appItems[position]
+        val appItem = filterAppItems[position]
         holder.appIcon.setImageDrawable(appItem.appIcon)
         holder.appName.text = appItem.appName
 
@@ -57,6 +64,60 @@ class ShowAllInstalledAppsAdapter(val appItems: List<AppItem>) :
                     ).apply {
                         TMP_DATA = appItem
                     })
+            }
+        }
+    }
+
+    /* *************************************************************************
+     *                                                                         *
+     * 根据搜索内容过滤当前列表                                                     *
+     *                                                                         *
+     **************************************************************************/
+    // 过滤后的列表。用于搜索功能
+    private var filterAppItems: List<AppItem> = appItems
+
+    // 是否搜索过(控制app展示区内容是否要还原)
+    var hasSearched = false
+    var lastSearchAppName = ""
+
+    override fun getFilter(): Filter {
+        return appItemFilter
+    }
+
+    private val appItemFilter by lazy {
+        object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val searchAppName = constraint.toString()
+
+                if (StringUtils.isEmpty(searchAppName)) {
+                    if (hasSearched) {
+                        hasSearched = false
+                        filterAppItems = appItems
+                        return FilterResults().apply {
+                            values = filterAppItems
+                        }
+                    }
+                } else if (searchAppName != lastSearchAppName) {
+                    val filterList = arrayListOf<AppItem>()
+                    appItems.forEach { appItem ->
+                        if (appItem.appName.contains(searchAppName, ignoreCase = true)) {
+                            filterList.add(appItem)
+                        }
+                    }
+                    filterAppItems = filterList
+                    hasSearched = true
+                    lastSearchAppName = searchAppName
+
+                    return FilterResults().apply {
+                        values = filterAppItems
+                    }
+                }
+                return FilterResults()
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                notifyDataSetChanged()
             }
         }
     }
