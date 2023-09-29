e584248b1a624e6dcb7fd7c01557e9b1ad4a251a
XingC <123fengmo@gmail.com>
2023-09-29 11:50:24 +0800

    fix: 修复配置文件不可读造成默认子进程白名单不生效的问题

--- app/src/main/kotlin/com/venus/backgroundopt/environment/CommonProperties.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/environment/CommonProperties.kt
@@ -3,22 +3,28 @@ package com.venus.backgroundopt.environment
 import com.venus.backgroundopt.entity.preference.SubProcessOomPolicy
 import com.venus.backgroundopt.environment.constants.PreferenceNameConstants
 import com.venus.backgroundopt.utils.preference.prefAll
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * @author XingC
  * @date 2023/9/26
  */
 object CommonProperties {
+    // 默认白名单
+    val subProcessNameDefaultUpgrade: Set<String> by lazy {
+        setOf(
+            "com.tencent.mobileqq:MSF", /* qq */
+            "com.tencent.mm:push", /* 微信 */
+        )
+    }
+
     // 子进程oom策略映射表
-    val subProcessOomPolicyMap: MutableMap<String, SubProcessOomPolicy>? by lazy {
-        prefAll<SubProcessOomPolicy>(PreferenceNameConstants.SUB_PROCESS_OOM_POLICY)?.also { map ->
-            // 默认白名单
-            setOf(
-                "com.tencent.mobileqq:MSF", /* qq */
-                "com.tencent.mm:push", /* 微信 */
-            ).forEach { processName ->
-                if (!map.containsKey(processName)) {
-                    map[processName] = SubProcessOomPolicy().apply {
+    val subProcessOomPolicyMap: MutableMap<String, SubProcessOomPolicy> by lazy {
+        (prefAll(PreferenceNameConstants.SUB_PROCESS_OOM_POLICY)
+            ?: ConcurrentHashMap<String, SubProcessOomPolicy>()).apply {
+            subProcessNameDefaultUpgrade.forEach { processName ->
+                if (!this.containsKey(processName)) {
+                    this[processName] = SubProcessOomPolicy().apply {
                         policyEnum = SubProcessOomPolicy.SubProcessOomPolicyEnum.MAIN_PROCESS
                     }
                 }
