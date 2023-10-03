1ef823c7e0e85f017fffca15a026a418802ad014
XingC <123fengmo@gmail.com>
2023-10-03 12:42:22 +0800

    refactor: ConcurrentUtils.lock的实现换回synchronized

--- app/src/main/kotlin/com/venus/backgroundopt/utils/concurrent/ConcurrentUtils.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/utils/concurrent/ConcurrentUtils.kt
@@ -5,11 +5,16 @@ package com.venus.backgroundopt.utils.concurrent
  * @date 2023/10/1
  */
 
-val synchronizedSet by lazy { ConcurrentHashSet<Any>() }
+//val synchronizedSet by lazy { ConcurrentHashSet<Any>() }
+// ConcurrentHashMap<锁标识物, 锁对象>
+// 不同锁标识物之间互不影响
+//val lockMap by lazy { ConcurrentHashMap<Any, Any>() }
 
 /**
- * 这只是保证一个代码块可以作为整体来运作。不会影响lock对象本身的锁
- * 由于先compute再remove, 在其间隙仍旧可能线程不安全。如果必须线程安全, 那就加锁
+ * 当前换用synchronized锁
+ *
+ * <u>这只是保证一个代码块可以作为整体来运作。不会影响lock对象本身的锁</u>
+ * <u>由于先compute再remove, 在其间隙仍旧可能线程不安全。如果必须线程安全, 那就加锁</u>
  *
  * @param lockFlag 竞争标识物
  * @param block 代码块
@@ -19,11 +24,14 @@ inline fun lock(
     lockFlag: Any,
     crossinline block: () -> Unit
 ) {
-    synchronizedSet.remove(
-        synchronizedSet.add(lockFlag) {
-            block()
-        }
-    )
+//    synchronizedSet.remove(
+//        synchronizedSet.add(lockFlag) {
+//            block()
+//        }
+//    )
+    synchronized(lockFlag) {
+        block()
+    }
 }
 
 /**
