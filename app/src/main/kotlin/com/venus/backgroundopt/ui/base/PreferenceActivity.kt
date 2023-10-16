ec8faf00336feaec055d448434d1a9b4924219bb
XingC <123fengmo@gmail.com>
2023-10-16 16:53:15 +0800

    feat: 增加Preference相关基础类

---
+++ app/src/main/kotlin/com/venus/backgroundopt/ui/base/PreferenceActivity.kt
@@ -0,0 +1,21 @@
+package com.venus.backgroundopt.ui.base
+
+import android.os.Bundle
+import com.venus.backgroundopt.ui.SettingsPreferenceFragment
+
+/**
+ * 需要实现设置页面的Activity可继承自本类
+ *
+ * @author XingC
+ * @date 2023/10/16
+ */
+abstract class PreferenceActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction()
+            .add(preferenceFragmentContainerResId(), SettingsPreferenceFragment()).commit()
+    }
+
+    abstract fun preferenceFragmentContainerResId(): Int
+}
