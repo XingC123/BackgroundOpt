a98b9abfe945406f76975b7b3b023e13b022a853
XingC <123fengmo@gmail.com>
2023-11-09 16:42:19 +0800

    refactor: 备份与恢复配置时使用另外的线程而不是放在主线程

--- app/src/main/kotlin/com/venus/backgroundopt/entity/AllPreference.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/entity/AllPreference.kt
@@ -4,6 +4,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import com.venus.backgroundopt.entity.preference.SubProcessOomPolicy
 import com.venus.backgroundopt.environment.constants.PreferenceNameConstants
+import com.venus.backgroundopt.environment.newThreadTask
 import com.venus.backgroundopt.utils.JsonUtils
 import com.venus.backgroundopt.utils.UiUtils
 import com.venus.backgroundopt.utils.message.handle.AppOptimizePolicyMessageHandler.AppOptimizePolicy
@@ -21,74 +22,72 @@ class AllPreference(private val activity: ComponentActivity) {
     private val backupLauncher =
         activity.registerForActivityResult(ActivityResultContracts.CreateDocument("application/json")) { uri ->
             uri ?: return@registerForActivityResult
-            runCatching {
-                activity.contentResolver.openOutputStream(uri).use { outputStream ->
-                    val context = activity.baseContext
-                    val jsonPreference = JsonPreference()
-                    jsonPreference.mainSettingsMap =
-                        context.prefAll(PreferenceNameConstants.MAIN_SETTINGS)
-                    jsonPreference.appOptimizePolicyMap =
-                        context.prefAll<AppOptimizePolicy>(PreferenceNameConstants.APP_OPTIMIZE_POLICY)
-                    jsonPreference.subProcessOomPolicyMap =
-                        context.prefAll<SubProcessOomPolicy>(PreferenceNameConstants.SUB_PROCESS_OOM_POLICY)
-                    val jsonString = JsonUtils.toJsonString(jsonPreference)
-                    outputStream?.write(jsonString.toByteArray(StandardCharsets.UTF_8)) ?: run {
-                        UiUtils.createDialog(activity.baseContext, text = "备份失败, 文件流为空").show()
+            newThreadTask {
+                runCatching {
+                    activity.contentResolver.openOutputStream(uri).use { outputStream ->
+                        val context = activity.baseContext
+                        val jsonPreference = JsonPreference()
+                        jsonPreference.mainSettingsMap =
+                            context.prefAll(PreferenceNameConstants.MAIN_SETTINGS)
+                        jsonPreference.appOptimizePolicyMap =
+                            context.prefAll<AppOptimizePolicy>(PreferenceNameConstants.APP_OPTIMIZE_POLICY)
+                        jsonPreference.subProcessOomPolicyMap =
+                            context.prefAll<SubProcessOomPolicy>(PreferenceNameConstants.SUB_PROCESS_OOM_POLICY)
+                        val jsonString = JsonUtils.toJsonString(jsonPreference)
+                        outputStream?.write(jsonString.toByteArray(StandardCharsets.UTF_8)) ?: run {
+                            showDialog("备份失败, 文件流为空")
+                        }
                     }
+                }.onFailure {
+                    showDialog("备份失败, 错误信息: ${it.message}")
+                }.onSuccess {
+                    showDialog("备份成功")
                 }
-            }.onFailure {
-                UiUtils.createDialog(
-                    activity.baseContext,
-                    text = "备份失败, 错误信息: ${it.message}"
-                ).show()
-            }.onSuccess {
-                UiUtils.createDialog(activity.baseContext, text = "备份成功").show()
             }
         }
 
     private val restoreLauncher =
         activity.registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
             uri ?: return@registerForActivityResult
+            newThreadTask {
+                runCatching {
+                    activity.contentResolver.openInputStream(uri)
+                        ?.reader(StandardCharsets.UTF_8).use { inputStream ->
+                            inputStream?.readText()
+                        }?.let { json ->
+                            val allPreference =
+                                JsonUtils.parseObject(json, JsonPreference::class.java)
+                            // 写入配置
+                            val context = activity.baseContext
 
-            runCatching {
-                activity.contentResolver.openInputStream(uri)
-                    ?.reader(StandardCharsets.UTF_8).use { inputStream ->
-                        inputStream?.readText()
-                    }?.let { json ->
-                        val allPreference = JsonUtils.parseObject(json, JsonPreference::class.java)
-                        // 写入配置
-                        val context = activity.baseContext
+                            context.prefEdit(
+                                PreferenceNameConstants.APP_OPTIMIZE_POLICY,
+                                commit = true
+                            ) {
+                                allPreference.appOptimizePolicyMap.forEach(this::putObject)
+                            }
 
-                        context.prefEdit(
-                            PreferenceNameConstants.APP_OPTIMIZE_POLICY,
-                            commit = true
-                        ) {
-                            allPreference.appOptimizePolicyMap.forEach(this::putObject)
-                        }
+                            context.prefEdit(
+                                PreferenceNameConstants.SUB_PROCESS_OOM_POLICY,
+                                commit = true
+                            ) {
+                                allPreference.subProcessOomPolicyMap.forEach(this::putObject)
+                            }
 
-                        context.prefEdit(
-                            PreferenceNameConstants.SUB_PROCESS_OOM_POLICY,
-                            commit = true
-                        ) {
-                            allPreference.subProcessOomPolicyMap.forEach(this::putObject)
-                        }
-
-                        context.prefEdit(PreferenceNameConstants.MAIN_SETTINGS, commit = true) {
-                            allPreference.mainSettingsMap.forEach { (prefKey, value) ->
-                                when (value) {
-                                    is Boolean -> putBoolean(prefKey, value)
-                                    is String -> putString(prefKey, value)
+                            context.prefEdit(PreferenceNameConstants.MAIN_SETTINGS, commit = true) {
+                                allPreference.mainSettingsMap.forEach { (prefKey, value) ->
+                                    when (value) {
+                                        is Boolean -> putBoolean(prefKey, value)
+                                        is String -> putString(prefKey, value)
+                                    }
                                 }
                             }
                         }
-                    }
-            }.onFailure {
-                UiUtils.createDialog(
-                    activity.baseContext,
-                    text = "恢复备份失败, 错误信息: ${it.message}"
-                ).show()
-            }.onSuccess {
-                UiUtils.createDialog(activity.baseContext, text = "恢复备份成功, 重启生效").show()
+                }.onFailure {
+                    showDialog("恢复备份失败, 错误信息: ${it.message}")
+                }.onSuccess {
+                    showDialog(text = "恢复备份成功, 重启生效")
+                }
             }
         }
 
@@ -99,6 +98,19 @@ class AllPreference(private val activity: ComponentActivity) {
 
     fun restore() {
         restoreLauncher.launch("application/json")
+    }
+
+    private fun showDialog(text: String) {
+        activity.runOnUiThread {
+            UiUtils.createDialog(
+                activity,
+                text = text,
+                enablePositiveBtn = true,
+                positiveBlock = {dialogInterface, _ ->
+                    dialogInterface.dismiss()
+                }
+            ).show()
+        }
     }
 
     class JsonPreference {
