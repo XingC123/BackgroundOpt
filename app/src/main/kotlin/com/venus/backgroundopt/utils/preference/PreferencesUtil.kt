cf5412d7d53d348c8016c37ba4695aa6fb0ed87f
XingC <123fengmo@gmail.com>
2024-03-10 17:03:48 +0800

    为java、kt代码添加许可证信息

--- app/src/main/kotlin/com/venus/backgroundopt/utils/preference/PreferencesUtil.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/utils/preference/PreferencesUtil.kt
@@ -1,4 +1,21 @@
-package com.venus.backgroundopt.utils.preference
+/*
+ * Copyright (C) 2023 BackgroundOpt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+                    
+ package com.venus.backgroundopt.utils.preference
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -164,4 +181,3 @@ fun Context.prefString(
 fun SharedPreferences.Editor.putObject(key: String, value: Any) {
     putString(key, JSON.toJSONString(value))
 }
-
