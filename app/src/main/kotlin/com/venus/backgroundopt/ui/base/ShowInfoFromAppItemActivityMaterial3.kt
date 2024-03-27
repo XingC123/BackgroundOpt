1426251471aa3dc7d15a4da1cfe683f51ea5c135
XingC <123fengmo@gmail.com>
2024-03-27 22:18:28 +0800

    fix(UI): 后台压缩/内存回收任务界面动态取色不生效

--- app/src/main/kotlin/com/venus/backgroundopt/ui/base/ShowInfoFromAppItemActivityMaterial3.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/ui/base/ShowInfoFromAppItemActivityMaterial3.kt
@@ -17,15 +17,67 @@
                     
  package com.venus.backgroundopt.ui.base
 
+import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.alibaba.fastjson2.JSON
 import com.venus.backgroundopt.R
+import com.venus.backgroundopt.entity.AppItem
+import com.venus.backgroundopt.entity.base.BaseProcessInfoKt
+import com.venus.backgroundopt.ui.style.RecycleViewItemSpaceDecoration
+import com.venus.backgroundopt.utils.PackageUtils
+import com.venus.backgroundopt.utils.getIntentData
+import com.venus.backgroundopt.utils.showProgressBarViewForAction
 
 /**
  * @author XingC
  * @date 2023/9/25
  */
-abstract class ShowInfoFromAppItemActivityMaterial3 : ShowInfoFromAppItemActivity() {
+abstract class ShowInfoFromAppItemActivityMaterial3 : BaseActivityMaterial3() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        showProgressBarViewForAction("正在加载...") {
+            init()
+        }
+    }
+
     override fun initToolBar(): Toolbar? {
         return findViewById(R.id.toolbar)
+    }
+
+    abstract fun getShowInfoAdapter(
+        appItems: List<AppItem>,
+        vararg others: Any?
+    ): ShowInfoFromAppItemAdapter
+
+    abstract fun getRecyclerViewResId(): Int
+
+    open fun getToolBarTitle(): String {
+        return ""
+    }
+
+    protected open fun init() {
+        val stringExtra = getIntentData(intent)
+        stringExtra ?: return
+
+        val list = JSON.parseArray(stringExtra, BaseProcessInfoKt::class.java)
+        list ?: return
+
+        val appItems = PackageUtils.getTargetApps(this, list)
+        initRecyclerView(appItems = appItems)
+    }
+
+    protected open fun initRecyclerView(appItems: List<AppItem>, vararg others: Any?) {
+        runOnUiThread {
+            findViewById<RecyclerView>(getRecyclerViewResId()).apply {
+                layoutManager = LinearLayoutManager(this@ShowInfoFromAppItemActivityMaterial3).apply {
+                    orientation = LinearLayoutManager.VERTICAL
+                }
+                adapter = getShowInfoAdapter(appItems, *others)
+                addItemDecoration(RecycleViewItemSpaceDecoration(context))
+            }
+        }
     }
 }
