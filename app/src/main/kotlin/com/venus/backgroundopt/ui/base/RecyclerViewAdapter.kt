632694de0dfe1e9307ced1b1b7aa84608e559987
XingC <123fengmo@gmail.com>
2024-06-01 16:37:39 +0800

    fix(UI):  更新已安装app列表时的抽搐现象

--- app/src/main/kotlin/com/venus/backgroundopt/ui/base/RecyclerViewAdapter.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/ui/base/RecyclerViewAdapter.kt
@@ -21,6 +21,8 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.venus.backgroundopt.utils.ne
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * 此[RecyclerView.Adapter]实现了滑动动画
@@ -31,14 +33,59 @@ import androidx.recyclerview.widget.RecyclerView
  * @date 2024/5/31
  */
 abstract class RecyclerViewAdapter<VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH>() {
-    /* *************************************************************************
-     *                                                                         *
-     * 滑动动画                                                                  *
-     *                                                                         *
-     **************************************************************************/
     override fun onViewAttachedToWindow(holder: VH) {
         super.onViewAttachedToWindow(holder)
-        addAnimation(holder)
+
+        handleAddScrollAnimation(holder)
+    }
+
+    override fun onViewDetachedFromWindow(holder: VH) {
+        super.onViewDetachedFromWindow(holder)
+
+        handleRemoveShowingViewHolder(holder)
+    }
+
+    /* *************************************************************************
+     *                                                                         *
+     * 列表滑动动画                                                              *
+     *                                                                         *
+     **************************************************************************/
+    // 正在显示的ViewHolder
+    private val showingViewHoldersMap = ConcurrentHashMap<Int, VH>(2)
+
+    /**
+     * 滑动[RecyclerView]时, item进入视野之前, [showingViewHoldersMap].get(position)返回的是null, 此时应该添加动画。
+     *
+     * 执行[notifyItemRangeChanged]以后:
+     * 1) [onViewAttachedToWindow]内的日志(多条日志):
+     *     - vh: ShowAllInstalledAppsViewHolder{4da7948 position=23 id=-1, oldPos=-1, pLpos:-1 scrap [changeScrap] update tmpDetached no parent}, adapterPosition: 23, needsUpdate: true
+     *     - vh: ShowAllInstalledAppsViewHolder{261367b position=23 id=-1, oldPos=-1, pLpos:-1}, adapterPosition: 23, needsUpdate: false
+     *
+     *     综上: 只在[showingViewHoldersMap].get(position)为null时再添加动画
+     * 2) [onViewDetachedFromWindow]内的日志:
+     *     -  vh: ShowAllInstalledAppsViewHolder{4da7948 position=23 id=-1, oldPos=-1, pLpos:-1 update}
+     *
+     * @param holder VH
+     */
+    private fun handleAddScrollAnimation(holder: VH) {
+        val adapterPosition = holder.adapterPosition
+        val lastVh = showingViewHoldersMap[adapterPosition]
+        /*
+         * 当前holder未处于显示状态, 则添加动画。
+         * 详细逻辑请看handleAddAnimation的文档注释
+         */
+        lastVh ?: addAnimation(holder)
+        lastVh.ne(holder) {
+            showingViewHoldersMap[adapterPosition] = holder
+        }
+    }
+
+    private fun handleRemoveShowingViewHolder(holder: VH) {
+        val adapterPosition = holder.adapterPosition
+        // 只有map内的holder和当前的一样, 才进行移除
+        if (showingViewHoldersMap[adapterPosition] == holder) {
+            showingViewHoldersMap.remove(adapterPosition)
+        }
     }
 
     private fun addAnimation(holder: VH) {
@@ -48,7 +95,7 @@ abstract class RecyclerViewAdapter<VH : RecyclerView.ViewHolder> : RecyclerView.
     }
 
     @SuppressLint("ObjectAnimatorBinding")
-    fun getAnimators(view: View?): Array<ObjectAnimator> {
+    private fun getAnimators(view: View?): Array<ObjectAnimator> {
         val scaleX = ObjectAnimator.ofFloat(view, "scaleX", 0f, 1f)
         val scaleY = ObjectAnimator.ofFloat(view, "scaleY", 0f, 1f)
         val alpha = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f)
