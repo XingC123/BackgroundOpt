a96f1ecf5336759288ad982dcb2087ad8c741526
XingC <123fengmo@gmail.com>
2024-07-23 10:54:43 +0800

    refactor: 在QueryInfoDialog中设置部分参数而不是query_info.xml

--- app/src/main/kotlin/com/venus/backgroundopt/app/ui/widget/QueryInfoDialog.kt
+++ app/src/main/kotlin/com/venus/backgroundopt/app/ui/widget/QueryInfoDialog.kt
@@ -18,14 +18,15 @@
 package com.venus.backgroundopt.app.ui.widget
 
 import android.content.Context
+import android.text.InputType
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.textfield.TextInputEditText
 import com.venus.backgroundopt.R
-import com.venus.backgroundopt.common.util.message.sendMessage
 import com.venus.backgroundopt.app.utils.UiUtils
+import com.venus.backgroundopt.common.util.message.sendMessage
 
 /**
  * 查询信息对话框工具类
@@ -33,103 +34,108 @@ import com.venus.backgroundopt.app.utils.UiUtils
  * @author XingC
  * @date 2023/9/23
  */
-class QueryInfoDialog {
-    companion object {
-        /* *************************************************************************
-         *                                                                         *
-         * 预定义方法                                                                *
-         *                                                                         *
-         **************************************************************************/
-        @JvmStatic
-        fun createQueryIntDataDialog(context: Context, messageKey: String): AlertDialog {
-            return createQueryInfoDialog(context) { alertDialog, _ ->
-                // 数据检查
-                val dataStr: String? = getQueryData(alertDialog)?.trim()
-                if (dataStr.isNullOrBlank()) {
-                    return@createQueryInfoDialog
-                }
+object QueryInfoDialog {
+    /* *************************************************************************
+     *                                                                         *
+     * 预定义方法                                                                *
+     *                                                                         *
+     **************************************************************************/
+    @JvmStatic
+    fun createQueryIntDataDialog(context: Context, messageKey: String): AlertDialog {
+        return createQueryInfoDialog(context) { alertDialog, _ ->
+            // 数据检查
+            val dataStr: String? = getQueryData(alertDialog)?.trim()
+            if (dataStr.isNullOrBlank()) {
+                return@createQueryInfoDialog
+            }
 
-                try {
-                    val data = dataStr.toInt()
-                    val message = sendMessage(alertDialog.context, messageKey, data)
-                    setMessage(alertDialog, message)
-                } catch (t: Throwable) {
+            try {
+                val data = dataStr.toInt()
+                val message = sendMessage(alertDialog.context, messageKey, data)
+                setMessage(alertDialog, message)
+            } catch (t: Throwable) {
 //                    Toast.makeText(context, "数据不合法", Toast.LENGTH_SHORT).show()
-                }
             }
         }
+    }
 
-        /* *************************************************************************
-         *                                                                         *
-         * 创建对话框                                                                *
-         *                                                                         *
-         **************************************************************************/
-        @JvmStatic
-        inline fun createQueryInfoDialog(
-            context: Context,
-            crossinline queryAction: (rootView: View, View) -> Unit,
-        ): AlertDialog {
-            return createQueryInfoDialog(context, null, null, queryAction)
-        }
+    /* *************************************************************************
+     *                                                                         *
+     * 创建对话框                                                                *
+     *                                                                         *
+     **************************************************************************/
+    @JvmStatic
+    inline fun createQueryInfoDialog(
+        context: Context,
+        crossinline queryAction: (rootView: View, View) -> Unit,
+    ): AlertDialog {
+        return createQueryInfoDialog(
+            context = context,
+            queryInfoEditTextHint = null,
+            queryAction = queryAction
+        )
+    }
 
-        /**
-         * 获取查询对话框
-         *
-         * @param context 发起者的上下文
-         * @param queryInfoTipText 查询的提示信息。null表示默认
-         * @param queryInfoEditTextHint 输入框的提示信息。null表示默认
-         * @param queryAction 查询动作
-         * @return 对话框
-         */
-        @JvmStatic
-        inline fun createQueryInfoDialog(
-            context: Context,
-            queryInfoTipText: String?,
-            queryInfoEditTextHint: String?,
-            crossinline queryAction: (rootView: View, View) -> Unit,
-        ): AlertDialog {
-            return UiUtils.createDialog(
-                context = context,
-                viewResId = R.layout.query_info,
-                viewBlock = {
-                    // 设置输入框的提示文本
-                    queryInfoEditTextHint?.let { str ->
-                        findViewById<TextView>(R.id.queryInfoEditText)?.text = str
-                    }
-                    // 设置按钮点击事件
-                    findViewById<Button>(R.id.doQueryBtn)?.setOnClickListener { view ->
-                        queryAction(rootView, view)
-                    }
+    /**
+     * 获取查询对话框
+     *
+     * @param context 发起者的上下文
+     * @param queryInfoEditTextHint 输入框的提示信息。null表示默认
+     * @param queryAction 查询动作
+     * @return 对话框
+     */
+    @JvmStatic
+    inline fun createQueryInfoDialog(
+        context: Context,
+        queryInfoEditTextHint: String?,
+        crossinline queryAction: (rootView: View, View) -> Unit,
+    ): AlertDialog {
+        return UiUtils.createDialog(
+            context = context,
+            viewResId = R.layout.query_info,
+            viewBlock = {
+                val queryInfoEditText = findViewById<TextView>(R.id.queryInfoEditText)
+                // 设置输入框的提示文本
+                queryInfoEditTextHint?.let { str ->
+                    queryInfoEditText.hint = str
+                } ?: queryInfoEditText.setHint(R.string.queryInfoEditTextHint)
+                // 输入内容的长度
+                queryInfoEditText.maxEms = 10
+                // 输入内容的类型
+                queryInfoEditText.inputType = InputType.TYPE_CLASS_NUMBER
+                // 设置按钮点击事件
+                findViewById<Button>(R.id.doQueryBtn)?.setOnClickListener { view ->
+                    queryAction(rootView, view)
                 }
-            )
-        }
+            }
+        )
+    }
 
-        /* *************************************************************************
-         *                                                                         *
-         * 数据的获取与设置                                                           *
-         *                                                                         *
-         **************************************************************************/
-        /**
-         * 获取要查询的数据
-         *
-         * @param rootView 所在对话框
-         * @return 字符串
-         */
-        @JvmStatic
-        fun getQueryData(rootView: View): String? {
-            return rootView.findViewById<TextInputEditText>(R.id.queryInfoEditText)?.text?.toString()
-                ?.trim()
-        }
+    /* *************************************************************************
+     *                                                                         *
+     * 数据的获取与设置                                                           *
+     *                                                                         *
+     **************************************************************************/
+    /**
+     * 获取要查询的数据
+     *
+     * @param rootView 所在对话框
+     * @return 字符串
+     */
+    @JvmStatic
+    fun getQueryData(rootView: View): String? {
+        return rootView.findViewById<TextInputEditText>(R.id.queryInfoEditText)?.text?.toString()
+            ?.trim()
+    }
 
-        /**
-         * 设置信息到查询对话框
-         *
-         * @param rootView 所在对话框
-         * @param message 要设置的信息
-         */
-        @JvmStatic
-        fun setMessage(rootView: View, message: String?) {
-            rootView.findViewById<TextView>(R.id.queryResultText)?.text = message
-        }
+    /**
+     * 设置信息到查询对话框
+     *
+     * @param rootView 所在对话框
+     * @param message 要设置的信息
+     */
+    @JvmStatic
+    fun setMessage(rootView: View, message: String?) {
+        rootView.findViewById<TextView>(R.id.queryResultText)?.text = message
     }
 }
