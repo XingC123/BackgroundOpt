ffcb9816ee8f1198a094c300dcaee5fe02bf46fd
XingC <123fengmo@gmail.com>
2025-04-26 15:10:04 +0800

    build: 升级FastJson2版本到2.0.57.android8

--- common/src/main/kotlin/com/venus/backgroundopt/common/util/JsonUtils.kt
+++ common/src/main/kotlin/com/venus/backgroundopt/common/util/JsonUtils.kt
@@ -29,7 +29,7 @@ import com.alibaba.fastjson2.filter.PropertyFilter
 class JsonUtils
 
 fun Any.toJsonString(): String {
-    return JSON.toJSONString(this, object : PropertyFilter {
+    return JSON.toJSONString(this/*, object : PropertyFilter {
         override fun apply(obj: Any?, name: String?, value: Any?): Boolean {
             /*
              * 若将被序列化的类中有自引用(字段或方法)会导致栈溢出(循环引用问题暂不用考虑), 从而无法成功序列化。
@@ -42,7 +42,7 @@ fun Any.toJsonString(): String {
              */
             return obj != value
         }
-    })
+    }*/)
 }
 
 fun <E> String.parseObject(clazz: Class<E>): E = JSON.parseObject(this, clazz)
